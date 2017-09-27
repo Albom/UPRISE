fff2a465d1705ef2ecc60e921bca5b22e45e5eab
Albom <albom85@yandex.ru>
2017-09-27 15:25:49 +0300

    Estimating parameters during geomagnetic storms

--- compile.bat
+++ compile.bat
@@ -50,6 +50,9 @@ fbc -s console -t 10000 -fpu sse  -arch 686  -O 3 -vec 1 3_UPRISE_estimate_FLIP_
 echo 3_UPRISE_estimate_FLIP_te.bas
 fbc -s console -t 10000 -fpu sse  -arch 686  -O 3 -vec 1 3_UPRISE_estimate_FLIP_te.bas
 
+echo 3_UPRISE_estimate_storm.bas
+fbc -s console -t 10000 -fpu sse  -arch 686  -O 3 -vec 1 3_UPRISE_estimate_storm.bas
+
 
 echo 3_UPRISE_velocity.bas
 fbc -s console -t 10000 -fpu sse  -arch 686  -O 3 -vec 1 3_UPRISE_velocity.bas
@@ -79,6 +82,7 @@ copy 3_UPRISE_estimate_wave.exe UPRISE
 copy 3_UPRISE_estimate_COld.exe UPRISE
 copy 3_UPRISE_estimate_FLIP_te_ti.exe UPRISE
 copy 3_UPRISE_estimate_FLIP_te.exe UPRISE
+copy 3_UPRISE_estimate_storm.exe UPRISE
 copy 3_UPRISE_velocity.exe UPRISE
 
 copy albom.dll UPRISE
